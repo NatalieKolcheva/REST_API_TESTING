7ad9625643bf4a55f115f11c1a6c87d822d687d4
NatalieKolcheva <natalykolcheva@gmail.com>
2021-08-10 11:27:14 +0300

    added test for get by id endpoint

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,9 +1,24 @@
+import org.apache.log4j.Logger;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import response.BaseResponse;
+import service.GenreService;
 
 public class MainTest {
 
-    @Test
-    public void main() {
+    private static final Logger LOG = Logger.getLogger(MainTest.class);
 
+    @Test
+    public void testGetByIdPositive() {
+        BaseResponse baseResponse = new GenreService().getGenre(493);
+        LOG.info("Got response: " + baseResponse.getBody());
+        Assert.assertEquals(baseResponse.getStatusCode(), 200);
+    }
+
+    @Test
+    public void testGetByIdNegative() {
+        BaseResponse baseResponse = new GenreService().getGenre(0);
+        LOG.info("Got response code: " + baseResponse.getStatusCode());
+        Assert.assertEquals(baseResponse.getStatusCode(), 404);
     }
 }
