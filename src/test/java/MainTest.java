a13f740017eb48e0ac11338d2bbe10ce6fbaa97d
NatalieKolcheva <natalykolcheva@gmail.com>
2021-08-11 20:56:09 +0300

    added test description

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -8,16 +8,16 @@ public class MainTest {
 
     private static final Logger LOG = Logger.getLogger(MainTest.class);
 
-    @Test
+    @Test (description = "Positive, GET Genre by ID")
     public void testGetByIdPositive() {
-        BaseResponse baseResponse = new GenreService().getGenre(493);
+        BaseResponse baseResponse = new GenreService().getGenre(9856);
         LOG.info("Got response: " + baseResponse.getBody());
         Assert.assertEquals(baseResponse.getStatusCode(), 200);
     }
 
-    @Test
+    @Test (description = "Negative, GET Genre by ID")
     public void testGetByIdNegative() {
-        BaseResponse baseResponse = new GenreService().getGenre(0);
+        BaseResponse baseResponse = new GenreService().getGenre(1010);
         LOG.info("Got response code: " + baseResponse.getStatusCode());
         Assert.assertEquals(baseResponse.getStatusCode(), 404);
     }
