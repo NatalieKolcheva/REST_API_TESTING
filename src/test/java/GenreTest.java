384f1006e1dc44728d6d880c2e3d63ee7cdf885d
NatalieKolcheva <natalykolcheva@gmail.com>
2021-08-11 21:29:21 +0300

    added error messages

--- src/test/java/GenreTest.java
+++ src/test/java/GenreTest.java
@@ -4,21 +4,21 @@ import org.testng.annotations.Test;
 import response.BaseResponse;
 import service.GenreService;
 
-public class MainTest {
+public class GenreTest {
 
-    private static final Logger LOG = Logger.getLogger(MainTest.class);
+    private static final Logger LOG = Logger.getLogger(GenreTest.class);
 
     @Test (description = "Positive, GET Genre by ID")
     public void testGetByIdPositive() {
         BaseResponse baseResponse = new GenreService().getGenre(9856);
         LOG.info("Got response: " + baseResponse.getBody());
-        Assert.assertEquals(baseResponse.getStatusCode(), 200);
+        Assert.assertEquals(baseResponse.getStatusCode(), 200, "Invalid status code.");
     }
 
     @Test (description = "Negative, GET Genre by ID")
     public void testGetByIdNegative() {
         BaseResponse baseResponse = new GenreService().getGenre(1010);
         LOG.info("Got response code: " + baseResponse.getStatusCode());
-        Assert.assertEquals(baseResponse.getStatusCode(), 404);
+        Assert.assertEquals(baseResponse.getStatusCode(), 404, "Invalid status code.");
     }
 }
